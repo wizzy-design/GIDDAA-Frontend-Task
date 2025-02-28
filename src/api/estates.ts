f4838f207354e8d0de3a699775bd8cb0d1654300
Wisdom Ochei <wisdomofgod2003@gmail.com>
2025-02-28 15:07:15 +0100

    Fixed create-estate form layout

--- src/api/estates.ts
+++ src/api/estates.ts
@@ -38,3 +38,59 @@ export const getEstateById = async (estateId: string) => {
     throw new Error("There was an error fetching Estate details");
   }
 };
+
+export const getCountries = async (
+  pageNumber: number,
+  pageSize: number,
+  searchTerm?: string
+) => {
+  try {
+    const searchQuery = searchTerm ? `&search=${searchTerm}` : "";
+    const response = await api.get(
+      `/public/country/get-all?pageSize=${pageSize}&pageNumber=${pageNumber}${searchQuery}`
+    );
+
+    return response.data.value;
+  } catch (error) {
+    console.log(error);
+    throw new Error("There was an error fetching Countries");
+  }
+};
+
+export const getStates = async (
+  countryId: string,
+  pageNumber: number,
+  pageSize: number,
+  searchTerm?: string
+) => {
+  try {
+    const searchQuery = searchTerm ? `&search=${searchTerm}` : "";
+    const response = await api.get(
+      `/public/state/${countryId}/get-all?pageSize=${pageSize}&pageNumber=${pageNumber}${searchQuery}`
+    );
+
+    return response.data.value;
+  } catch (error) {
+    console.log(error);
+    throw new Error("There was an error fetching States");
+  }
+};
+
+export const getCities = async (
+  stateId: string,
+  pageNumber: number,
+  pageSize: number,
+  searchTerm?: string
+) => {
+  try {
+    const searchQuery = searchTerm ? `&search=${searchTerm}` : "";
+    const response = await api.get(
+      `/public/city/${stateId}/get-all?pageSize=${pageSize}&pageNumber=${pageNumber}${searchQuery}`
+    );
+
+    return response.data.value;
+  } catch (error) {
+    console.log(error);
+    throw new Error("There was an error fetching Cities");
+  }
+};
--- src/app/(app)/create-estate/page.tsx
+++ src/app/(app)/create-estate/page.tsx
@@ -1,25 +1,28 @@
 "use client";
 
 import EstateHeader from "@/components/estates/EstateHeader";
-import React from "react";
+import React, { useState } from "react";
 import { HiPlus } from "react-icons/hi";
+import { useQuery } from "@tanstack/react-query";
+import { getCountries, getCities, getStates } from "@/api/estates";
+import Image from "next/image";
 
 export default function CreateEstate() {
   return (
     <div className="min-h-screen">
       <EstateHeader pageTitle="Creating Estate" subPageTitle="Create Estate" />
 
-      <div className="bg-white p-6 rounded-lg mx-auto">
+      <div className="mx-auto rounded-lg bg-white p-6">
         <ImageUploader />
         <EstateForm />
       </div>
 
-      <div className="w-full bg-[#F0F0F0] py-5 fixed bottom-0">
-        <div className="w-full  flex translate-x-[34%] gap-3">
-          <button className="px-4 py-2 rounded-[100px] font-bold border text-[#346633] border-solid border-[#346633]">
+      <div className="fixed bottom-0 w-full bg-[#F0F0F0] py-5">
+        <div className="flex w-full translate-x-[34%] gap-3">
+          <button className="rounded-[100px] border border-solid border-[#346633] px-4 py-2 font-bold text-[#346633]">
             Cancel
           </button>
-          <button className="px-4 py-2  bg-[#346633] text-white font-bold rounded-[100px]">
+          <button className="rounded-[100px] bg-[#346633] px-4 py-2 font-bold text-white">
             Create Estate
           </button>
         </div>
@@ -29,19 +32,101 @@ export default function CreateEstate() {
 }
 
 function ImageUploader() {
+  const [images, setImages] = useState<File[]>([]);
+
+  const handleImageUpload = (event: React.ChangeEvent<HTMLInputElement>) => {
+    if (event.target.files) {
+      const uploadedImages = Array.from(event.target.files);
+      setImages((prevImages) => [...prevImages, ...uploadedImages]);
+    }
+  };
+
   return (
-    <div className="border-2 border-dashed p-6 rounded-lg flex flex-col items-center mb-6 gap-2">
-      <div className="bg-[linear-gradient(0deg,#335F32,#335F32),linear-gradient(0deg,rgba(0,0,0,0.2),rgba(0,0,0,0.2))] p-1 rounded-xl">
-        <HiPlus size={40} className="text-white" />
+    <>
+      <div className="mb-6 flex flex-col items-center gap-2 rounded-lg border-2 border-dashed p-6">
+        <label className="cursor-pointer">
+          <input
+            type="file"
+            accept="image/*"
+            multiple
+            className="hidden"
+            onChange={handleImageUpload}
+          />
+          <div className="rounded-xl bg-[linear-gradient(0deg,#335F32,#335F32),linear-gradient(0deg,rgba(0,0,0,0.2),rgba(0,0,0,0.2))] p-1">
+            <HiPlus size={40} className="text-white" />
+          </div>
+          <p className="text-xs font-bold capitalize text-[#335F32]">
+            add estate images
+          </p>
+        </label>
       </div>
-      <p className="capitalize font-bold text-[#335F32] text-xs">
-        add estate images
-      </p>
-    </div>
+
+      <div className="mt-4 flex gap-2">
+        {images.map((image, index) => (
+          <div key={index} className="group relative max-w-[60px]">
+            <div className="relative">
+              <Image
+                src={URL.createObjectURL(image)}
+                alt={`Preview ${index}`}
+                className="h-16 w-16 rounded-[5px] object-cover"
+                width={20}
+                height={20}
+              />
+              <div className="absolute inset-0 bg-black opacity-0 transition-opacity group-hover:opacity-50"></div>
+              <div className="absolute left-0 top-0 flex h-full w-full items-center justify-center opacity-0 transition-opacity group-hover:opacity-100">
+                <div className="flex gap-2">
+                  <div className="flex h-[19px] w-[19px] cursor-pointer items-center justify-center rounded-full bg-white">
+                    <Image
+                      src="/editEstate.svg"
+                      alt="Edit"
+                      width={8.769230842590332}
+                      height={8.769230842590332}
+                    />
+                  </div>
+                  <div className="flex h-[19px] w-[19px] cursor-pointer items-center justify-center rounded-full bg-white">
+                    <Image
+                      src="/deleteEstate.svg"
+                      alt="Delete"
+                      width={7.125}
+                      height={9.5}
+                    />
+                  </div>
+                </div>
+              </div>
+            </div>
+            <p className="mt-1 text-center text-xs font-bold text-[#335F32]">
+              {image.name.length > 15 ? `${image.name.substring(0, 12)}...` : image.name}
+            </p>
+          </div>
+        ))}
+      </div>
+    </>
   );
 }
 
 function EstateForm() {
+  const [selectedCountryId, setSelectedCountryId] = useState<string | null>(
+    null
+  );
+  const [selectedStateId, setSelectedStateId] = useState<string | null>(null);
+
+  const { data: countries, isLoading: countriesLoading } = useQuery({
+    queryKey: ["countries"],
+    queryFn: () => getCountries(1, 855),
+  });
+
+  const { data: states, isLoading: statesLoading } = useQuery({
+    queryKey: ["states", selectedCountryId],
+    queryFn: () => getStates(selectedCountryId || "", 1, 100),
+    enabled: !!selectedCountryId,
+  });
+
+  const { data: cities, isLoading: citiesLoading } = useQuery({
+    queryKey: ["cities", selectedStateId],
+    queryFn: () => getCities(selectedStateId || "", 1, 100),
+    enabled: !!selectedStateId,
+  });
+
   return (
     <form className="grid grid-cols-4 gap-4">
       <div>
@@ -51,9 +136,34 @@ function EstateForm() {
         </label>
         <input
           type="text"
-          className="w-full py-2 px-3 border rounded-[100px] mt-1 text-xs"
+          className="mt-1 w-full rounded-[100px] border px-3 py-2 text-xs"
           required
         />
+      </div>
+
+      <div>
+        <label className="block text-sm font-medium">
+          Country
+          <span className="text-[#E40000]">*</span>
+        </label>
+        <select
+          className="mt-1 w-full rounded-[100px] border px-3 py-2 text-xs"
+          required
+          onChange={(e) => setSelectedCountryId(e.target.value)}
+        >
+          <option value="">Select a country</option>
+          {countriesLoading ? (
+            <option>Loading...</option>
+          ) : (
+            countries?.value?.data?.map(
+              (country: { id: string; name: string }) => (
+                <option key={country.id} value={country.id}>
+                  {country.name}
+                </option>
+              )
+            )
+          )}
+        </select>
       </div>
 
       <div>
@@ -62,14 +172,20 @@ function EstateForm() {
           <span className="text-[#E40000]">*</span>
         </label>
         <select
-          className="w-full py-2 px-3 border rounded-[100px] mt-1 text-xs"
+          className="mt-1 w-full rounded-[100px] border px-3 py-2 text-xs"
           required
+          onChange={(e) => setSelectedStateId(e.target.value)}
         >
-          {["Germany", "USA"].map((opt) => (
-            <option key={opt} value={opt}>
-              {opt}
-            </option>
-          ))}
+          <option value="">Select a state</option>
+          {statesLoading ? (
+            <option>Loading...</option>
+          ) : (
+            states?.value?.data?.map((state: { id: string; name: string }) => (
+              <option key={state.id} value={state.id}>
+                {state.name}
+              </option>
+            ))
+          )}
         </select>
       </div>
 
@@ -79,14 +195,19 @@ function EstateForm() {
           <span className="text-[#E40000]">*</span>
         </label>
         <select
-          className="w-full py-2 px-3 border rounded-[100px] mt-1 text-xs"
+          className="mt-1 w-full rounded-[100px] border px-3 py-2 text-xs"
           required
         >
-          {["Berlin", "Hamburg"].map((opt) => (
-            <option key={opt} value={opt}>
-              {opt}
-            </option>
-          ))}
+          <option value="">Select a city</option>
+          {citiesLoading ? (
+            <option>Loading...</option>
+          ) : (
+            cities?.value?.data?.map((city: { id: string; name: string }) => (
+              <option key={city.id} value={city.name}>
+                {city.name}
+              </option>
+            ))
+          )}
         </select>
       </div>
 
@@ -97,7 +218,7 @@ function EstateForm() {
         </label>
         <input
           type="text"
-          className="w-full py-2 px-3 border rounded-[100px] mt-1 text-xs"
+          className="mt-1 w-full rounded-[100px] border px-3 py-2 text-xs"
           required
         />
       </div>
@@ -107,7 +228,7 @@ function EstateForm() {
         <input
           type="text"
           placeholder="What's the asking price of this property?"
-          className="w-full py-2 px-3 border rounded-[100px] mt-1 text-xs"
+          className="mt-1 w-full rounded-[100px] border px-3 py-2 text-xs"
         />
       </div>
 
@@ -118,7 +239,7 @@ function EstateForm() {
         </label>
         <input
           type="text"
-          className="w-full py-2 px-3 border rounded-[100px] mt-1 text-xs"
+          className="mt-1 w-full rounded-[100px] border px-3 py-2 text-xs"
           required
         />
       </div>
@@ -129,11 +250,14 @@ function EstateForm() {
           <span className="text-[#E40000]">*</span>
         </label>
         <select
-          className="w-full py-2 px-3 border rounded-[100px] mt-1 text-xs"
+          className="mt-1 w-full rounded-[100px] border px-3 py-2 text-xs"
           required
         >
           {["Completed", "Ongoing"].map((opt) => (
-            <option key={opt} value={opt}>
+            <option
+              key={opt}
+              value={opt === "Completed" ? "COMPLETED" : "UNDER_CONSTRUCTION"}
+            >
               {opt}
             </option>
           ))}
@@ -145,7 +269,7 @@ function EstateForm() {
         <input
           type="text"
           placeholder="Add a Youtube Video Link"
-          className="w-full py-2 px-3 border rounded-[100px] mt-1 text-xs"
+          className="mt-1 w-full rounded-[100px] border px-3 py-2 text-xs"
         />
       </div>
 
@@ -153,18 +277,18 @@ function EstateForm() {
         <label className="block text-sm font-medium">Number of Floors</label>
         <input
           type="number"
-          className="w-full py-2 px-3 border rounded-[100px] mt-1 text-xs"
+          className="mt-1 w-full rounded-[100px] border px-3 py-2 text-xs"
         />
       </div>
 
-      <div className="col-span-4 ">
+      <div className="col-span-4 mb-32">
         <label className="block text-sm font-medium">
           Description
           <span className="text-[#E40000]">*</span>
         </label>
         <textarea
-          className="w-full py-2 px-3 border rounded-lg mt-1 text-xs"
-          rows={3}
+          className="mt-1 w-full rounded-lg border px-3 py-2 text-xs"
+          rows={4}
           required
         ></textarea>
       </div>
